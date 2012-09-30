cc2172caced09dc83788102400764fc4623e40d3
Roman Cheplyaka <roma@ro-che.info>
2012-09-30 15:49:31 +0300

    Allow testing a string, plus accept custom comparators

--- Test/Framework/Providers/Golden.hs
+++ Test/Framework/Providers/Golden.hs
@@ -8,41 +8,67 @@
 
 -}
 module Test.Framework.Providers.Golden
-  ( golden )
+  ( goldenVsFile
+  , goldenVsString
+  )
   where
 
 import Test.Framework.Providers.API
 import Text.Printf
 import Data.Maybe
 import Data.ByteString.Lazy as LB
+import Data.ByteString.Lazy.Char8 as LBC
 import System.IO
 import Control.Exception
 
+data TestedOutput
+  = FileOutput
+    { outputFile :: FilePath
+    , genOutput :: IO ()
+    }
+  | StringOutput
+    { outputString :: IO ByteString }
+
 data Golden = Golden
-  { goldenRef, goldenNew :: FilePath
-  , goldenAction :: IO ()
+  { goldenFile :: FilePath
+  , testedOutput :: TestedOutput
+  , comparator :: ByteString -> ByteString -> Bool
   }
 
--- | Create a test-framework test based on a «golden» file
-golden
+-- | Compare a given file contents against the golden file contents
+goldenVsFile
   :: TestName -- ^ test name
+  -> (ByteString -> ByteString -> Bool) -- ^ comparison function (e.g. ('==') or 'eqIgnoringWS')
   -> FilePath -- ^ path to the «golden» file (the file that contains correct output)
   -> FilePath -- ^ path to the output file
   -> IO () -- ^ action that creates the output file
-  -> Test -- ^ test verifies that the output file is the same as the golden file
-golden name ref new act = Test name $ Golden ref new act
+  -> Test -- ^ the test verifies that the output file contents is the same as the golden file contents
+goldenVsFile name cmp ref new act = Test name $ Golden ref (FileOutput new act) cmp
+
+-- | Compare a given string against the golden file contents
+goldenVsString
+  :: TestName -- ^ test name
+  -> (ByteString -> ByteString -> Bool) -- ^ comparison function (e.g. '==' or 'eqIgnoreSpaces')
+  -> FilePath -- ^ path to the «golden» file (the file that contains correct output)
+  -> IO ByteString -- ^ action that returns a string
+  -> Test -- ^ the test verifies that the returned string is the same as the golden file contents
+goldenVsString name cmp ref act = Test name $ Golden ref (StringOutput act) cmp
 
 data Result
     = Timeout
     | Pass
-    | Differ FilePath FilePath
+    | FileDiffers FilePath FilePath
+    | StringDiffers ByteString FilePath
     | NoNew IOException
     | NoGolden IOException
 
 instance Show Result where
     show Timeout  = "Timed out"
     show Pass     = "OK"
-    show (Differ ref new) = printf "Files '%s' and '%s' differ" new ref
+    show (FileDiffers new ref) =
+      printf "Files '%s' and '%s' differ" new ref
+    show (StringDiffers str ref) =
+      printf "Test output was different from '%s'. It was:\n---\n%s\n---" ref $ LBC.unpack str
     show (NoNew ex) =
       printf "Could not read test output: %s" $ show ex
     show (NoGolden ex) =
@@ -67,20 +93,19 @@ instance Testlike TestCaseRunning Result Golden where
         return $ fromMaybe Timeout mb_result
 
 runGolden :: Golden -> ImprovingIO TestCaseRunning f Result
-runGolden (Golden ref new act) = do
+runGolden (Golden ref tst cmp) = do
   yieldImprovement TestCaseRunning
-  liftIO $ do
-    act
-    compareFiles ref new
+  liftIO $ compareStuff cmp ref tst
 
-compareFiles :: FilePath -> FilePath -> IO Result
-compareFiles ref new =
-  withFile NoGolden ref $ \h1 ->
-  withFile NoNew    new $ \h2 ->
+compareStuff :: (ByteString -> ByteString -> Bool) -> FilePath -> TestedOutput -> IO Result
+compareStuff cmp ref tst =
+  withTestedOutput tst $ \strNew differ ->
+  withFile NoGolden ref $ \h ->
   do
-    [cts1, cts2] <- mapM LB.hGetContents [h1, h2]
+    strGolden <- LB.hGetContents h
     -- force the result while the handles are open
-    evaluate $ if cts1 == cts2 then Pass else Differ ref new
+    evaluate $
+      if strGolden `cmp` strNew then Pass else differ ref
   where
   withFile
     :: (IOException -> Result)
@@ -92,3 +117,16 @@ compareFiles ref new =
       (try $ openBinaryFile path ReadMode)
       (either (const $ return ()) hClose)
       (either (return . wrapException) act)
+
+  withTestedOutput
+    :: TestedOutput
+    -> (ByteString -> (FilePath -> Result) -> IO Result)
+    -> IO Result
+  withTestedOutput (FileOutput path act) proceed = do
+    act
+    withFile NoNew path $ \h -> do
+      cts <- LB.hGetContents h
+      proceed cts $ FileDiffers path
+  withTestedOutput (StringOutput act) proceed = do
+    result <- act
+    proceed result $ StringDiffers result
