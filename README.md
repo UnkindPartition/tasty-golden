a90827a68dcfc0fe5b2ac9b569a1e24275d0ae5f
Roman Cheplyaka <roma@ro-che.info>
2012-09-30 11:13:16 +0300

    Add description

---
+++ README.md
@@ -0,0 +1,6 @@
+This package provides support for «golden testing».
+
+A golden test is an IO action that writes its result to a file.
+To pass the test, this output file should be identical to the corresponding
+«golden» file, which contains the correct result for the test.
+
--- Test/Framework/Providers/Golden.hs
+++ Test/Framework/Providers/Golden.hs
@@ -1,4 +1,12 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
+{- |
+  This package provides support for «golden testing».
+
+  A golden test is an IO action that writes its result to a file.
+  To pass the test, this output file should be identical to the corresponding
+  «golden» file, which contains the correct result for the test.
+
+-}
 module Test.Framework.Providers.Golden
   ( golden )
   where
