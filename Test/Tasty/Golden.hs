52d7b14f4c3dc8849cc3c565bd2da79c5cd4ec31
Roman Cheplyaka <roma@ro-che.info>
2014-03-02 18:07:27 +0200

    Document the line endings issue

--- Test/Tasty/Golden.hs
+++ Test/Tasty/Golden.hs
@@ -9,7 +9,39 @@ test@ or @cabal install --enable-tests@), so if your test files are under
 the @tests\/@ subdirectory, your relative file names should start with
 @tests\/@ (even if your @test.hs@ is itself under @tests\/@, too).
 
+Note about line endings. The best way to avoid headaches with line endings
+(when running tests both on UNIX and Windows) is to treat your golden files
+as binary, even when they are actually textual.
+
+This means:
+
+* When writing output files from Haskell code, open them in binary mode
+(see 'openBinaryFile', 'withBinaryFile' and 'hSetBinaryMode'). This will
+disable automatic @\\n -> \\r\\n@ conversion on Windows. For convenience, this
+module exports 'writeBinaryFile' which is just like `writeFile` but opens
+the file in binary mode. When using 'ByteString's note that
+"Data.ByteString" and "Data.ByteString.Lazy" use binary mode for
+@writeFile@, while "Data.ByteString.Char8" and "Data.ByteString.Lazy.Char8"
+use text mode.
+
+* Tell your VCS not to do any newline conversion for golden files. For
+ git check in a @.gitattributes@ file with the following contents (assuming
+ your golden files have @.golden@ extension):
+
+>*.golden	-text
+
+On its side, tasty-golden reads and writes files in binary mode, too.
+
+Why not let Haskell/git do automatic conversion on Windows? Well, for
+instance, @tar@ will not do the conversion for you when unpacking a release
+tarball, so when you run @cabal install your-package --enable-tests@, the
+tests will be broken.
+
+As a last resort, you can strip all @\\r@s from both arguments in your
+comparison function when necessary. But most of the time treating the files
+as binary does the job.
 -}
+
 module Test.Tasty.Golden
   ( goldenVsFile
   , goldenVsString
