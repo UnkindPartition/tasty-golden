b160ce3f75cd2f2e793f40a88515d33fd9d01b97
Roman Cheplyaka <roma@ro-che.info>
2015-02-15 15:57:38 +0200

    Move accepting tests from ingredient to tests themselves

--- Test/Tasty/Golden/Internal.hs
+++ Test/Tasty/Golden/Internal.hs
@@ -9,7 +9,11 @@ import Control.Exception
 import Data.Typeable (Typeable)
 import Data.ByteString.Lazy as LB
 import System.IO
+import Options.Applicative
+import Data.Tagged
+import Data.Proxy
 import Test.Tasty.Providers
+import Test.Tasty.Options
 
 -- | See 'goldenTest' for explanation of the fields
 data Golden =
@@ -20,6 +24,22 @@ data Golden =
       (a -> a -> IO (Maybe String))
       (a -> IO ())
   deriving Typeable
+
+-- | This option, when set to 'True', specifies that we should run in the
+-- «accept tests» mode
+newtype AcceptTests = AcceptTests Bool
+  deriving (Eq, Ord, Typeable)
+instance IsOption AcceptTests where
+  defaultValue = AcceptTests False
+  parseValue = fmap AcceptTests . safeRead
+  optionName = return "accept"
+  optionHelp = return "Accept current results of golden tests"
+  optionCLParser =
+    fmap AcceptTests $
+    switch
+      (  long (untag (optionName :: Tagged AcceptTests String))
+      <> help (untag (optionHelp :: Tagged AcceptTests String))
+      )
 
 -- | An action that yields a value (either golden or tested).
 --
@@ -46,21 +66,27 @@ vgRun :: ValueGetter r r -> IO r
 vgRun (ValueGetter a) = runContT a evaluate
 
 instance IsTest Golden where
-  run _ golden _ = runGolden golden
-  testOptions = return []
+  run opts golden _ = runGolden golden (lookupOption opts)
+  testOptions = return [Option (Proxy :: Proxy AcceptTests)]
 
-runGolden :: Golden -> IO Result
-runGolden (Golden getGolden getTested cmp _) = do
+runGolden :: Golden -> AcceptTests -> IO Result
+runGolden (Golden getGolden getTested cmp update) (AcceptTests accept) = do
   vgRun $ do
     new <- getTested
     ref <- getGolden
     result <- liftIO $ cmp ref new
 
     case result of
+      Just _reason | accept -> do
+        -- test failed; accept the new version
+        liftIO $ update new
+        return $ testPassed "Accepted the new version"
+
       Just reason -> do
         -- Make sure that the result is fully evaluated and doesn't depend
         -- on yet un-read lazy input
         liftIO $ evaluate . rnf $ reason
         return $ testFailed reason
+
       Nothing ->
         return $ testPassed ""
--- Test/Tasty/Golden/Manage.hs
+++ Test/Tasty/Golden/Manage.hs
@@ -1,5 +1,11 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving, PatternGuards, DeriveDataTypeable #-}
--- | Golden test management
+-- | Previously, accepting tests (by the @--accept@ flag) was done by this
+-- module, and there was an ingredient to handle that mode.
+--
+-- Now it's done as part of a normal test run. When the `--accept` flag is
+-- given, it makes golden tests to update the files whenever there is
+-- a mismatch. So you no longer need this module. It remains only for
+-- backwards compatibility.
 module Test.Tasty.Golden.Manage
   (
   -- * Command line helpers
@@ -8,84 +14,19 @@ module Test.Tasty.Golden.Manage
   -- * The ingredient
   , acceptingTests
   , AcceptTests(..)
-
-  -- * Programmatic API
-  , acceptGoldenTests
   )
   where
 
 import Test.Tasty hiding (defaultMain)
 import Test.Tasty.Runners
-import Test.Tasty.Options
 import Test.Tasty.Golden.Internal
-import Data.Typeable
-import Data.Tagged
-import Data.Proxy
-import Data.Maybe
-import Control.Monad.Cont
-import Control.Monad.State
-import Control.Exception
-import Control.Concurrent.Async
-import Text.Printf
-import Options.Applicative
 
--- | Like @defaultMain@ from the main tasty package, but also includes the
--- golden test management capabilities.
+-- | This exists only for backwards compatibility. Use
+-- 'Test.Tasty.defaultMain' instead.
 defaultMain :: TestTree -> IO ()
 defaultMain = defaultMainWithIngredients [acceptingTests, listingTests, consoleTestReporter]
 
--- | This option, when set to 'True', specifies that we should run in the
--- «accept tests» mode
-newtype AcceptTests = AcceptTests Bool
-  deriving (Eq, Ord, Typeable)
-instance IsOption AcceptTests where
-  defaultValue = AcceptTests False
-  parseValue = fmap AcceptTests . safeRead
-  optionName = return "accept"
-  optionHelp = return "Accept current results of golden tests"
-  optionCLParser =
-    fmap AcceptTests $
-    switch
-      (  long (untag (optionName :: Tagged AcceptTests String))
-      <> help (untag (optionHelp :: Tagged AcceptTests String))
-      )
-
+-- | This exists only for backwards compatibility. You don't need to
+-- include this anymore.
 acceptingTests :: Ingredient
-acceptingTests = TestManager [Option (Proxy :: Proxy AcceptTests)] $
-  \opts tree ->
-    case lookupOption opts of
-      AcceptTests False -> Nothing
-      AcceptTests True -> Just $
-        acceptGoldenTests opts tree
-
--- | Get the list of all golden tests in a given test tree
-getGoldenTests :: OptionSet -> TestTree -> [(TestName, Golden)]
-getGoldenTests =
-  foldTestTree
-    trivialFold { foldSingle = \_ name t -> fmap ((,) name) $ maybeToList $ cast t }
-
--- | «Accept» a golden test, i.e. reset the golden value to the currently
--- produced value
-acceptGoldenTest :: Golden -> IO ()
-acceptGoldenTest (Golden _ getTested _ update) =
-  vgRun $ liftIO . update =<< getTested
-
--- | Accept all golden tests in the test tree
-acceptGoldenTests :: OptionSet -> TestTree -> IO Bool
-acceptGoldenTests opts tests = do
-  let gs = getGoldenTests opts tests
-  numExns <- flip execStateT (0 :: Int) $ forM_ gs $ \(n,g) -> do
-    mbExn <- liftIO $ withAsync (acceptGoldenTest g) waitCatch
-    case mbExn of
-      Right {} -> liftIO $ printf "Accepted %s\n" n
-      Left e -> do
-        liftIO $ printf "Error when trying to accept %s: %s\n" n (show (e :: SomeException))
-        ne <- get
-        put $! ne+1
-
-  -- warn when there were problems
-  when (numExns > 0) $
-    printf "NOTE: %d tests threw exceptions!\n" numExns
-
-  -- is everything ok?
-  return (numExns == 0)
+acceptingTests = TestManager [] $ \_ _ -> Nothing
