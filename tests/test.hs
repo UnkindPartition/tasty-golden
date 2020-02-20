b8e66f2d7b411daaeb1e048e8b883400c2fd4465
Roman Cheplyaka <roma@ro-che.info>
2020-02-20 23:22:14 +0200

    test.hs: do not change the directory

--- tests/test.hs
+++ tests/test.hs
@@ -12,18 +12,16 @@ main = defaultMain $
   testCase "findByExtension" $
     withSystemTempDirectory "golden-test" $ \basedir -> do
 
-      setCurrentDirectory basedir
+      createDirectory (basedir </> "d1")
+      createDirectory (basedir </> "d1" </> "d2")
+      touch (basedir </> "f1.c")
+      touch (basedir </> "f2.h")
+      touch (basedir </> "f2.exe")
+      touch (basedir </> "d1" </> "g1.c")
+      touch (basedir </> "d1" </> "d2" </> "h1.c")
+      touch (basedir </> "d1" </> "d2" </> "h1.exe")
+      touch (basedir </> "d1" </> "d2" </> "h1")
 
-      createDirectory ("d1")
-      createDirectory ("d1" </> "d2")
-      touch ("f1.c")
-      touch ("f2.h")
-      touch ("f2.exe")
-      touch ("d1" </> "g1.c")
-      touch ("d1" </> "d2" </> "h1.c")
-      touch ("d1" </> "d2" </> "h1.exe")
-      touch ("d1" </> "d2" </> "h1")
-
-      files <- findByExtension [".c", ".h"] "."
-      sort files @?= sort
-        ["./d1/d2/h1.c","./d1/g1.c","./f1.c","./f2.h"]
+      files <- findByExtension [".c", ".h"] basedir
+      sort files @?= (sort . map (basedir </>))
+        ["d1/d2/h1.c","d1/g1.c","f1.c","f2.h"]
