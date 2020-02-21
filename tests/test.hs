70827953ce909d1db4593e03bbef7a2ef252effa
Roman Cheplyaka <roma@ro-che.info>
2020-02-21 10:31:13 +0200

    Add golden tests for the example suite

--- tests/test.hs
+++ tests/test.hs
@@ -1,15 +1,18 @@
+{-# LANGUAGE CPP #-}
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.Golden
 import System.IO.Temp
 import System.FilePath
 import System.Directory
+import System.Process
 import Data.List (sort)
 
 touch f = writeFile f ""
+diff ref new = ["diff", "-u", ref, new]
 
-main = defaultMain $
-  testCase "findByExtension" $
+main = defaultMain $ testGroup "Tests"
+  [ testCase "findByExtension" $
     withSystemTempDirectory "golden-test" $ \basedir -> do
 
       createDirectory (basedir </> "d1")
@@ -25,3 +28,53 @@ main = defaultMain $
       files <- findByExtension [".c", ".h"] basedir
       sort files @?= (sort . map (basedir </>))
         ["d1/d2/h1.c","d1/g1.c","f1.c","f2.h"]
+#ifdef BUILD_EXAMPLE
+  , withResource
+    (do
+      tmp0 <- getCanonicalTemporaryDirectory
+      tmp <- createTempDirectory tmp0 "golden-test"
+      callProcess "cp" ["-r", "example", tmp]
+      return tmp
+    )
+    ({-removeDirectoryRecursive-}const $ return ()) $ \tmpIO ->
+    testGroup "Example test suite"
+    [ goldenVsFileDiff
+        "before --accept"
+        diff
+        "tests/golden/before-accept.golden"
+        "tests/golden/before-accept.actual"
+        (do
+          tmp <- tmpIO
+          our <- getCurrentDirectory
+          -- The sed invocation is used to get rid of the differences
+          -- caused by random file names in goldenVsStringDiff and by the
+          -- timings.
+          --
+          -- NB: cannot use multiline literals because of CPP.
+          callCommand ("cd " ++ tmp ++ " && example | " ++
+            "sed -Ee 's/[[:digit:]-]+\\.actual/.actual/g; s/ \\([[:digit:].]+s\\)//' > " ++
+            our</>"tests/golden/before-accept.actual || true")
+        )
+    , after AllFinish "/before --accept/" $ goldenVsFileDiff
+        "with --accept"
+        diff
+        "tests/golden/with-accept.golden"
+        "tests/golden/with-accept.actual"
+        (do
+          tmp <- tmpIO
+          our <- getCurrentDirectory
+          callCommand ("cd " ++ tmp ++ " && example --accept | sed -Ee 's/ \\([[:digit:].]+s\\)//' > " ++ our </>"tests/golden/with-accept.actual")
+        )
+    , after AllFinish "/with --accept/" $ goldenVsFileDiff
+        "after --accept"
+        diff
+        "tests/golden/after-accept.golden"
+        "tests/golden/after-accept.actual"
+        (do
+          tmp <- tmpIO
+          our <- getCurrentDirectory
+          callCommand ("cd " ++ tmp ++ " && example | sed -Ee 's/ \\([[:digit:].]+s\\)//' > " ++ our</>"tests/golden/after-accept.actual")
+        )
+    ]
+#endif
+  ]
